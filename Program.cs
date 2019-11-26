1ddb10a0d5604295309870dffa73200644acc65f
kkdosk <kkdosk@naver.com>
2019-11-26 23:14:05 +0900

    local static function

--- Program.cs
+++ Program.cs
@@ -6,6 +6,14 @@ namespace first
 		public RGBColor(Int16 R, Int16 G, Int16 B) {
 
 		}
+
+		int M()
+		{
+			int y = 5;
+			int x = 7;
+			return Add(x, y);
+			static int Add(int left, int right) => left + right;
+		}
 	}
 
 	public struct Address {
